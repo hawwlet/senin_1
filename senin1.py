4a9b60c666e4cc86dd7d7e25e88f95285e404b1d
rizaldani07@gmail.com <hari.rofalon.geous@gmail.com>
2021-04-19 12:47:43 +0800

    senin ke 1

---
+++ senin1.py
@@ -0,0 +1,55 @@
+umur = 28
+tinggi =175
+txt = "Nama saya Rizal rachmadani , Umur {} tahun, tinggi {} cm".format( umur, tinggi )
+print(txt)
+
+
+
+a  =   70 
+b =  70 
+if not b > a:
+    print ("selamat anda lulus" )
+else :
+    print ("tidak lulus")
+
+
+
+a = 70 
+b = 68
+if not a < b:
+    print ("anda harus mengulang ujian praktek")
+else :
+    print ("anda lulus")
+
+
+
+a = 68
+b = 70
+if not a > b:
+    print ("anda harus mengulang ujian teori")
+else :
+    print ("anda lulus") 
+
+
+
+a = 70
+b = 68
+if not b < a:
+    print ("anda lulus")
+else :
+    print ("anda harus mengulang ujian teori dan praktek")
+ 
+
+
+ 
+phi = 22/7
+r = 10
+L = 22/7*(10*10)
+print (L)
+
+
+
+phi = 22/7
+r = 7
+L = 22/7*(7*7)
+print (L)
